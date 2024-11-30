c33335add636b9fca7c13a3996db36c0cc2d2e3c
waelhym <37433514+waelhym@users.noreply.github.com>
2024-11-30 21:17:21 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ PORT=$2
 CHAT=$3
 
 # Clone Odoo directory
-git clone --depth=1 https://github.com/waelhym/odoo-18-docker-compose $DESTINATION
+git clone --depth=1 https://github.com/waelhym/odoo-18-docker_compose $DESTINATION
 rm -rf $DESTINATION/.git
 
 # Create PostgreSQL directory
